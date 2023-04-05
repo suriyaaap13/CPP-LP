4f34517710a51516a71e54911bb985fbf0cfe550
Suriyaa <suriyaaap13@gmail.com>
2023-04-05 07:41:42 +0530

    Getting the set up done

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Footer from './components/Footer';
+import Exchange from './components/Exchange';
+
+export default function App() {
+  return (
+    <div>
+
+      {/* Hero Section Header or Heading */}
+
+      {/* Component to show data in alternating manner */}
+      <Exchange/>
+
+      {/* Together we code better Section */}
+
+      {/* Partners Section */}
+
+      {/* Community Section */}
+
+      {/* FAQs Accordion Section */}
+
+      {/* Contact Us Section */}
+
+      {/* Footer */}
+      <Footer/>
+
+    </div>
+  )
+}
